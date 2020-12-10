b9a2bfa5fc96419c795897019632d3a76dc582d6
Natasha Clements <73941569+p110095617@users.noreply.github.com>
2020-12-10 14:24:47 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from 'react-router-dom'
+ 
+import { Header, Footer, Section, Middle, PageTitle } from './components/common'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+        <Switch>
+        <Route path="/Welcome to UCP">
+            <PageTitle title="Welcome to UCP"/>
+          </Route>
+
+          <Route path="/Courses">
+            <PageTitle title="Courses"/>
+          </Route>
+
+          <Route path="/Contact Us">
+            <PageTitle title="Contact Us"/>
+          </Route>
+
+          <Route path="/Login">
+            <PageTitle title="Login"/>
+          </Route>
+        </Switch>
+
+      <Section />
+      <Middle />
+  
+      <Footer />
+    </div>
+  );
+}
+export default App;
+
